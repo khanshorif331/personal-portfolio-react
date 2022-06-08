67502e78d44bedb8d4593d364baeae7c0667cbc5
khanshorif331 <khanshorif331@gmail.com>
2022-06-09 04:19:02 +0600

    added react typed

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
-import React, { useCallback } from 'react';
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import logo from './logo.svg';
-import './App.css';
-import particlesOptions from "./particles.json";
+import React, { useCallback } from 'react'
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
+import logo from './logo.svg'
+import './App.css'
+import particlesOptions from './particles.json'
+import Navbar from './components/Shared/Navbar'
+import ReactTyped from 'react-typed'
 
 function App() {
-    const particlesInit = useCallback(main => {
-        loadFull(main);
-    }, [])
+	const particlesInit = useCallback(main => {
+		loadFull(main)
+	}, [])
 
-    return (
-        <div className="App">
-            <Particles options={particlesOptions} init={particlesInit}/>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
-            </header>
-        </div>
-    );
+	return (
+		<div className='App'>
+			<Particles options={particlesOptions} init={particlesInit} />
+			<Navbar></Navbar>
+			<ReactTyped
+				className='text-red-500'
+				strings={['Here you can find anything']}
+				typeSpeed={40}
+			/>
+			<br />
+
+			{/* <div className='text-white bg-red-400'> */}
+			<ReactTyped
+				className='text-2xl text-red-500'
+				strings={['I am Developer', 'I am Learner', 'I am Programmer']}
+				typeSpeed={40}
+				backSpeed={50}
+				attr='placeholder'
+				loop
+			>
+				{/* <p className='text-white'> hello</p> */}
+				<input type='text' className='text-red-500 bg-black text-center' />
+			</ReactTyped>
+			<br />
+			<ReactTyped
+				className='text-2xl text-red-500'
+				strings={['I am Developer', 'I am Learner', 'I am Programmer']}
+				typeSpeed={40}
+				backSpeed={50}
+				attr='placeholder'
+				loop
+			>
+				{/* <p className='text-white'> hello</p> */}
+				<input type='text' className='text-red-500 bg-black text-center' />
+			</ReactTyped>
+			{/* </div> */}
+		</div>
+	)
 }
 
-export default App;
+export default App
