b98c19c00cd8f1a2ad4c34ea6be6a21357d52dd6
khanshorif331 <khanshorif331@gmail.com>
2022-06-13 02:04:28 +0600

    updated bug

--- src/App.js
+++ src/App.js
@@ -21,15 +21,14 @@ import Projects from './pages/Home/Projects/Projects'
 import Blogs from './pages/Blogs/Blogs'
 import Footer from './components/Shared/Footer'
 import Loading from './components/Loading'
-
 function App() {
-	// const [loading, setLoading] = useState(false)
-	// useEffect(() => {
-	// 	setLoading(true)
-	// 	setTimeout(() => {
-	// 		setLoading(false)
-	// 	}, 4000)
-	// }, [])
+	const [loading, setLoading] = useState(false)
+	useEffect(() => {
+		setLoading(true)
+		setTimeout(() => {
+			setLoading(false)
+		}, 4000)
+	}, [loading])
 	const particlesInit = useCallback(main => {
 		loadFull(main)
 	}, [])
